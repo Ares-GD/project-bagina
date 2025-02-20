e1714fee58b0aec9d50c753813008b0fdfe485fd
Ares-GladkihD <ares-gd@yandex.ru>
2025-02-20 22:53:29 +0500

    ТЕСТ 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import PreHeader from './components/PreHeader'; 
+import Header from './components/Header'; 
+import BgndVideo from './components/BgndVideo'; 
+import IntroSection from './components/IntroSection'; 
+import ScheduleWrap from './components/ScheduleWrap'; 
+import SiteSection from './components/SiteSection'; 
+import ClassesSection from './components/ClassesSection';
+import TrainerSection from './components/TrainerSection'; 
+import ServicesSection from './components/ServicesSection'; 
+import ContactSection from './components/ContactSection'; 
+import Footer from './components/Footer'; 
 
 const App = () => {
   return (
-      
+    <div className="site-wrap" data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+      {/* PreHeader */}
+      <PreHeader />
+
+      {/* Header */}
+      <Header />
+
+      {/* BgndVideo */}
+      <BgndVideo />
+
+      {/* IntroSection */}
+      <IntroSection />
+
+      {/* ScheduleWrap */}
+      <ScheduleWrap />
+
+      {/* SiteSection */}
+      <SiteSection />
+
+      {/* ClassesSection */}
+      <ClassesSection />
+
+      {/* TrainerSection */}
+      <TrainerSection />
+
+      {/* ServicesSection */}
+      <ServicesSection />
+
+      {/* ContactSection */}
+      <ContactSection />
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Ensure the container exists in the DOM
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element in the DOM.");
+}
+
+// Create a root and render the App component
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-reportWebVitals();
